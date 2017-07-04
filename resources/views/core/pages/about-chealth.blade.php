a3706ef5868e47ee25fbca8e4d7d6c2ac1dbcc11
Valentine <valentinedeveloper@gmail.com>
2017-07-04 16:09:46 +0300

    Update cHealth Version

--- resources/views/core/pages/about-chealth.blade.php
+++ resources/views/core/pages/about-chealth.blade.php
@@ -23,9 +23,9 @@
 @section('body')
 	<div class="padded-full">
 		<div class="padded-full text-center">
-			<h5 style="padding-top: 25px;"><strong>{{\Tremby\LaravelGitVersion\GitVersionHelper::getVersion() }}:</strong></h5>
+			<h5 style="padding-top: 25px;"><strong>{{\Tremby\LaravelGitVersion\GitVersionHelper::getNameAndVersion() }}:</strong></h5>
 			<p>Copyright © {{ $year }}. Cimplicity Apps</p>
-			<p>Website: <a href="http://www.chealth.io">www.chealth.io</a></p>
+			<p>Website: <a href="http://www.chealth.io" target="_blank">www.chealth.io</a></p>
 			<p>cHealth is released under the cHealth license which can be found <a href="{{ url('chealth-license') }}">here</a>.</p>
 		</div>
 		
